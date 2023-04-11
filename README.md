136dd66be632a86ee2b82544782f3ca668d829b8
jesandante007 <jesandante007@gmail.com>
2023-04-11 11:14:50 +0600

    final commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Job Hunter
+
+Best online platform for find your dream job.
+
+
+## Live Link
+Hosted in Netlify -> [HeroGadget](https://herogadget.netlify.app/)
+
+
+## SPA (Single Page Application)
+Optimistic performance
+
+## Eyes soothing UI
+Beautiful design & color combination
+
+## Great UX
+Live statistics, Toast notification
--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
-  const { error, status, statusText } = useRouteError();
+  const { error, status } = useRouteError();
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="text-center text-5xl font-semibold space-y-10">
-        <p>{status || statusText}</p>
+        <p>{status || 404}</p>
         <p>{error?.message}</p>
         <Link to='/' className="block">
           <button className="btn-primary">Back to Home</button>
